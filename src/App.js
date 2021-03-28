3a7e0cce5698743987f2c97365e54a8f446d40d9
AliKamal <alikamal2381@gmail.com>
2021-03-29 01:05:09 +0500

    fourth-commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React from "react";
 import "./App.css";
-//import { UserForm } from "./components/UserForm";
-import Stepper from "./stepper";
+import { UserForm } from "./components/UserForm";
+//import Stepper from "./stepper";
 
 const App = () => {
   return (
     <div className="App">
       <h1>Working Multi Form using Formik & Material UI</h1>
 
-      <Stepper />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
+      {/* <Stepper /> */}
 
-      {/*  <UserForm /> */}
+      <UserForm />
     </div>
   );
 };
